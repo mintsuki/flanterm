c74ce1a017c1e73236de3ed0a00e77bbf28bf842
Mintsuki <mintsuki@protonmail.com>
2026-02-09 11:34:09 +0100

    Fix ESC D and ESC E to only scroll when cursor is within scroll region

--- src/flanterm.c
+++ src/flanterm.c
@@ -1027,7 +1027,7 @@ static void escape_parse(struct flanterm_context *ctx, uint8_t c) {
             if (y == ctx->scroll_bottom_margin - 1) {
                 ctx->scroll(ctx);
                 ctx->set_cursor_pos(ctx, x, y);
-            } else {
+            } else if (y < ctx->rows - 1) {
                 ctx->set_cursor_pos(ctx, x, y + 1);
             }
             break;
@@ -1035,8 +1035,10 @@ static void escape_parse(struct flanterm_context *ctx, uint8_t c) {
             if (y == ctx->scroll_bottom_margin - 1) {
                 ctx->scroll(ctx);
                 ctx->set_cursor_pos(ctx, 0, y);
-            } else {
+            } else if (y < ctx->rows - 1) {
                 ctx->set_cursor_pos(ctx, 0, y + 1);
+            } else {
+                ctx->set_cursor_pos(ctx, 0, y);
             }
             break;
         case 'M':
